f4688401419c0936aa53aac65a173a1b07b2f6b9
Baremantar <61024510+Baremantar@users.noreply.github.com>
2021-04-03 19:55:47 +0300

    First upload and creating site
    v1

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  $(".slider").slick({
+    arrows: true,
+    dots: true,
+    adaptiveHeight: true,
+    centerMode: true,
+    variableWidth: true,
+    speed: 1000,
+    easing:'ease',
+    autoplay: true,
+    autoplaySpeed:3000,
+  });
+});
