4c591e9cf5e9ef7a944de5c9b4670264e40d528f
Baremantar <61024510+Baremantar@users.noreply.github.com>
2021-04-09 15:51:09 +0300

    add animation

---
+++ RTprops_v2/js/main.js
@@ -0,0 +1,66 @@
+$(document).ready(function () {
+  $(".slider").slick({
+    arrows: true,
+    adaptiveHeight: true,
+    centerMode: true,
+    variableWidth: true,
+    speed: 1000,
+    easing: "ease",
+    autoplay: true,
+    autoplaySpeed: 3000,
+  });
+});
+$(document).ready(function () {
+  $(".modelling__slider").slick({
+    arrows: false,
+    cssEase: 'linear',
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  });
+});
+$(document).ready(function () {
+  $(".print__slider").slick({
+    arrows: false,
+    cssEase: 'linear',
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  });
+});
+$(document).ready(function () {
+  $(".prep__slider").slick({
+    arrows: false,
+    cssEase: 'linear',
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  });
+});
+$(document).ready(function () {
+  $(".end__slider").slick({
+    arrows: false,
+    cssEase: 'linear',
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  });
+});
+
+$(window).resize(function () {
+  if ($(window).width() < 789) {
+    $('.slider').slick('unslick');
+  } else {
+    $(document).ready(function () {
+    $(".slider").slick({
+      arrows: true,
+      adaptiveHeight: true,
+      centerMode: true,
+      variableWidth: true,
+      speed: 1000,
+      easing: "ease",
+      autoplay: true,
+      autoplaySpeed: 3000,
+    });
+  });}
+});
