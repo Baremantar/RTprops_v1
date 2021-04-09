ff69d18d5f26a6388ac64ae55d4a091b38616d49
Baremantar <61024510+Baremantar@users.noreply.github.com>
2021-04-09 15:51:44 +0300

    Delete RTprops_v2 directory

--- RTprops_v2/js/header.js
+++
@@ -1,9 +0,0 @@
-$(".header__burger").click(function () {
-  $(".header__burger, .header__menu").toggleClass("active");
-  $("body").toggleClass("lock");
-});
-
-$(".header__menu").click(function () {
-  $(".header__burger, .header__menu").removeClass("active");
-  $("body").removeClass("lock");
-});
